59792ff7451c58316b042c1f9b4b614cf2e97435
VolkerSchiewe <volker.s1994@gmail.com>
2017-10-26 12:53:46 +0200

    Parse comments, save to csv

---
+++ CsvHelper.py
@@ -0,0 +1,14 @@
+import csv
+
+
+class CsvHelper:
+
+    @staticmethod
+    def write_comments(file_name, comments):
+        comments = [comment.__dict__ for comment in comments]
+        with open('comments.csv', newline='', mode='w') as csvfile:
+            writer = csv.DictWriter(csvfile, comments[0].keys())
+            writer.writeheader()
+
+            for comment in comments:
+                writer.writerow(comment)
