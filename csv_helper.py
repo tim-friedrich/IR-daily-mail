f52bb58fe083f3ade05c53ab2c8a486d7e217395
VolkerSchiewe <volker.s1994@gmail.com>
2017-11-10 12:47:35 +0100

    crawl articles until yesterday, update articles if crawles twice, improve __main__.py, crawl comments based on articles

--- csv_helper.py
+++ csv_helper.py
@@ -9,15 +9,41 @@ class CsvHelper:
         if not file_name.startswith('out/'):
             file_name = 'out/' + file_name
 
-        object_list = [object.__dict__ for object in object_list]
+        object_list = [element.__dict__ for element in object_list]
         if not object_list:
             return
 
         file_exists = os.path.isfile(file_name)
-        with open(file_name, newline='', mode='a', encoding='utf-8') as csvfile:
-            writer = csv.DictWriter(csvfile, object_list[0].keys(), quoting=csv.QUOTE_ALL)
+        with open(file_name, newline='', mode='a', encoding='utf-8') as csv_file:
+            writer = csv.DictWriter(csv_file, object_list[0].keys(), quoting=csv.QUOTE_ALL)
             if not file_exists:
                 writer.writeheader()
 
-            for object in object_list:
-                writer.writerow(object)
+            for element in object_list:
+                writer.writerow(element)
+
+    @staticmethod
+    def read_object_list(file_name: str, object_class):
+        if not file_name.startswith('out/'):
+            file_name = 'out/' + file_name
+
+        if not os.path.isfile(file_name):
+            raise FileNotFoundError('File {} does not exists'.format(file_name))
+        with open(file_name, newline='', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)  # skip header
+            for row in reader:
+                yield object_class(*row)
+
+    @staticmethod
+    def get_file_length(file_name):
+        if not file_name.startswith('out/'):
+            file_name = 'out/' + file_name
+
+        if not os.path.isfile(file_name):
+            raise FileNotFoundError('File {} does not exists'.format(file_name))
+
+        with open(file_name, newline='', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            file_length = sum(1 for row in reader)
+        return file_length
--- spiders/article_spider.py
+++ spiders/article_spider.py
@@ -1,4 +1,5 @@
 import re
+from abc import ABC
 from datetime import datetime
 
 from scrapy.linkextractors import LinkExtractor
@@ -6,29 +7,57 @@ from scrapy.spiders import CrawlSpider, Rule
 
 from csv_helper import CsvHelper
 from models.article import Article
+from utils import date_range, extract_date
 
 
-class ArticleSpider(CrawlSpider):
+class ArticleSpider(CrawlSpider, ABC):
     name = 'dailymail sitemap'
     start_urls = ['http://www.dailymail.co.uk/home/sitemaparchive/index.html']
-    rules = (
-        # find months
-        Rule(LinkExtractor(allow='sitemaparchive/month',
-                           restrict_xpaths='//ul[@class="archive-index home link-box"]')),
-        # find days
-        Rule(LinkExtractor(allow='sitemaparchive/day',
-                           restrict_xpaths='//div[contains(@class, "debate column-split")]'), callback='parse_article'),
-    )
 
     def parse_article(self, response):
         if response.status is not 200:
             print('Response Code was {}'.format(response.status))
             return
-        url = response.url
 
         urls = response.xpath('//ul[@class="archive-articles debate link-box"]//li//@href').extract()
         for url in urls:
             article_date = datetime.strptime(re.search('(?<=day_)\w*', response.url).group(), '%Y%m%d')
-            article = Article(url, article_date)
-            print('Date:{}, URL: {}'.format(article_date, article.article_url))
+            article_id = re.search('(?<=article-)\w*', url).group()
+            article = Article(article_id, url, article_date)
+            print('Date: {}, URL: {}'.format(article_date, article.article_url))
             CsvHelper.write_object_list(Article.get_csv_file_name(), [article])
+
+    @staticmethod
+    def get_update_rules():
+        allowed_dates = []
+
+        latest_file = Article.get_latest_file()
+        date_of_latest_file = extract_date(latest_file, 'article-')
+        if not date_of_latest_file:
+            return allowed_dates
+
+        for single_date in date_range(date_of_latest_file, datetime.today()):
+            allowed_dates.append(single_date.strftime("%Y%m%d"))
+
+        return (
+            Rule(LinkExtractor(allow='sitemaparchive/month',
+                               restrict_xpaths='//ul[@class="archive-index home link-box"]')),
+            # find days except today to have a clear knowledge what has been crawled
+            Rule(LinkExtractor(allow=allowed_dates,
+                               restrict_xpaths='//div[contains(@class, "debate column-split")]'),
+                 callback='parse_article'),
+        )
+
+
+class InitialArticleSpider(ArticleSpider):
+    rules = (Rule(LinkExtractor(allow='sitemaparchive/month',
+                                restrict_xpaths='//ul[@class="archive-index home link-box"]')),
+             # find days except today to have a clear knowledge what has been crawled
+             Rule(LinkExtractor(allow='sitemaparchive/day', deny=datetime.today().strftime('%Y%m%d'),
+                                restrict_xpaths='//div[contains(@class, "debate column-split")]'),
+                  callback='parse_article'),
+             )
+
+
+class UpdateArticleSpider(ArticleSpider):
+    rules = ArticleSpider.get_update_rules()
